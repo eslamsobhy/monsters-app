0fbfa23e7d762c0af83b7a89f34d34ed4e50fe4b
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-01 22:21:32 +0200

    made the CardList component responsible for mapping over the data

--- src/components/card-list/CardList.jsx
+++ src/components/card-list/CardList.jsx
@@ -4,7 +4,14 @@ import React from "react";
 import "./card-list.styles.css";
 
 const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => {
+        const { id, name, email } = monster;
+        return <h1 key={id}>{name}</h1>;
+      })}
+    </div>
+  );
 };
 
 export default CardList;
