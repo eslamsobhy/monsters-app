d6a817979f2760a0b5167ce89bbe716bce03f10f
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-06 20:14:48 +0200

    used bind to fix the this-object context bug

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ class App extends Component {
       monsters: [],
       searchField: "",
     };
+
+    // we need to do this for every method we declare in order for us to fix the this object context bug!
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value });
   }
 
   render() {
@@ -31,7 +38,7 @@ class App extends Component {
       <div className="App">
         <SearchBox
           placeholder="search monster"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
